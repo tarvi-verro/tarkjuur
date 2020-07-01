56997d5da061734977f9b8ca0aff1a9ecb2889bb
Tarvi Verro <tarvi.verro@gmail.com>
2020-07-01 17:55:22 +0000

    More resilient data gathering init

--- data-gathering/gather-data.sh
+++ data-gathering/gather-data.sh
@@ -9,8 +9,13 @@ ln -s "$f" "$latest"
 # Configure tty
 stty -F /dev/ttyUSB0 -raw -icrnl -onlcr -echo
 
-# Give the measure command
+# Reset by sending ^C
+printf "\3" >/dev/ttyUSB0
+sleep 0.1
+
+# Give the measure command and skip the header line
 printf "measure\r\n" >/dev/ttyUSB0
+grep -q '^Vref' /dev/ttyUSB0
 
 # Reading from tty
 cat /dev/ttyUSB0 | ts '%.s' >> "$f"
